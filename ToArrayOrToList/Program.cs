a52c33fb18839d15e8be9a4f9b999f6bc369bbd1
suremaker <wojciech.kotlarski@gmail.com>
2021-07-19 17:22:57 +0100

    Added more cases

--- ToArrayOrToList/Program.cs
+++ ToArrayOrToList/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 namespace ToArrayOrToList
@@ -12,7 +15,7 @@ namespace ToArrayOrToList
         [ArgumentsSource(nameof(SourceEnumerables))]
         public object[] ToArray(Source source) => source.Enumerable.ToArray();
 
-        [Benchmark]
+        [Benchmark(Baseline = true)]
         [ArgumentsSource(nameof(SourceEnumerables))]
         public List<object> ToList(Source source) => source.Enumerable.ToList();
 
@@ -22,11 +25,24 @@ namespace ToArrayOrToList
             foreach (var count in counts)
             {
                 // Scenarios...
-                yield return new Source(nameof(Array), count, Array(count));
+                yield return new Source("Array", count, Array(count));
+                yield return new Source("Array.Select", count, Array(count).Select(o => o));
+                yield return new Source("Array.Where", count, Array(count).Where(o => true));
+                yield return new Source("Array.Take", count, Array(count).Take(count / 2));
+                yield return new Source("Array.Skip", count, Array(count).Skip(count / 2));
+                yield return new Source("Array.Cast", count, Array(count).Cast<object>());
+                yield return new Source("Array.OfType", count, Array(count).OfType<object>());
+                yield return new Source("Yield", count, Yield(Array(count)));
+                yield return new Source("Complex", count, Array(count).Where(o => true).Select(o => o).Skip(2).Take(count / 2));
             }
         }
-
         public object[] Array(int count) => Enumerable.Range(0, count).Select(_ => new object()).ToArray();
+
+        public IEnumerable<object> Yield(object[] data)
+        {
+            foreach (var d in data)
+                yield return d;
+        }
     }
 
     public class Source
